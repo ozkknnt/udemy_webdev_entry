4e80e05fdc6995c5fc485e0c116c82049d786432
ozkknnt <25022317+ozkknnt@users.noreply.github.com>
2020-06-29 23:12:31 +0900

    rails入門　完了

--- ruby_docker/ruby/rails_project/todo/app/controllers/tasks_controller.rb
+++ ruby_docker/ruby/rails_project/todo/app/controllers/tasks_controller.rb
@@ -17,6 +17,34 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      redirect_to root_path
+    else
+      render 'edit'
+    end 
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to root_path
+  end
+
+  def toggle
+    # render nothing: true
+    head :no_content
+    @task = Task.find(params[:id])
+    @task.done = !@task.done
+    @task.save
+  end
+
   private
     def task_params
       params[:task].permit(:title)
